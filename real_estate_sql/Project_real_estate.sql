3d14924450c3ebb09b1a3ff0a8b555751133f3c3
Екатерина Лощинская <katerina.sergeevna.sh@gmail.com>
2025-09-22 19:50:17 +0400

    Update Project_real_estate.sql

--- real_estate_sql/Project_real_estate.sql
+++ real_estate_sql/Project_real_estate.sql
@@ -1,32 +1,4 @@
 
--- Определим аномальные значения (выбросы) по значению перцентилей:
-/*WITH limits AS (
-    SELECT
-        PERCENTILE_DISC(0.99) WITHIN GROUP (ORDER BY total_area) AS total_area_limit,
-        PERCENTILE_DISC(0.99) WITHIN GROUP (ORDER BY rooms) AS rooms_limit,
-        PERCENTILE_DISC(0.99) WITHIN GROUP (ORDER BY balcony) AS balcony_limit,
-        PERCENTILE_DISC(0.99) WITHIN GROUP (ORDER BY ceiling_height) AS ceiling_height_limit_h,
-        PERCENTILE_DISC(0.01) WITHIN GROUP (ORDER BY ceiling_height) AS ceiling_height_limit_l
-    FROM real_estate.flats
-),
--- Найдем id объявлений, которые не содержат выбросы:
-filtered_id AS(
-    SELECT id
-    FROM real_estate.flats  
-    WHERE 
-        total_area < (SELECT total_area_limit FROM limits)
-        AND (rooms < (SELECT rooms_limit FROM limits) OR rooms IS NULL)
-        AND (balcony < (SELECT balcony_limit FROM limits) OR balcony IS NULL)
-        AND ((ceiling_height < (SELECT ceiling_height_limit_h FROM limits)
-            AND ceiling_height > (SELECT ceiling_height_limit_l FROM limits)) OR ceiling_height IS NULL)
-    )
--- Выведем объявления без выбросов:
-SELECT *
-FROM real_estate.flats
-WHERE id IN (SELECT * FROM filtered_id);*/
-
-
-
 
 -- Определим аномальные значения (выбросы) по значению перцентилей:
 WITH limits AS (
@@ -281,6 +253,7 @@ SELECT cte.city,
 FROM cte 
 ORDER BY cte.total_count DESC, cte.sold_share DESC 
 LIMIT 15
+
 
 
 
